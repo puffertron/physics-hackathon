8b43b66c8cf6adc27e4c38c7f54a95a6f4583c71
Stillmahn <milesethanstillie@gmail.com>
2022-11-05 18:22:58 -0400

    Edits to calculation file, should be usable for simulation

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("hello world")
+import calculations as c
+
+
+def test(function, expected):
+    if (function == expected):
+        return print("pass")
+    else:
+        return print("fail")
+
+
+print(c.amplitude(4, 8), c.distance(1, 1, 0),c.angle(4,2))
