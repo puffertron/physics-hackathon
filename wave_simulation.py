269ca7f781c66dd279cc74d6d1d7477a2203fcea
Konicai <71294714+konicai@users.noreply.github.com>
2022-11-05 20:54:08 -0400

    Update wave simulation

--- wave_simulation.py
+++ wave_simulation.py
@@ -3,6 +3,8 @@ import dataclasses
 from ursina import *
 from parameters import Parameters
 
+def doNothing(): pass
+
 app = Ursina(title="Wave Simulation")
 ed = EditorCamera
 
@@ -16,10 +18,15 @@ def simulate():
 
 
 if __name__ == "__main__":
-    simulateButton = Button("Start Simulation")
+
+    paramPanel = WindowPanel(model=Plane, scale=(0.5, 1, 0), position=(-.8, 0.5, 0), color=color.white)
+    paramPanel.on_mouse_exit = doNothing
+    paramPanel.on_mouse_enter = doNothing
+
+    simulateButton = Button("Start Simulation", parent=paramPanel, scale=0.1)
     simulateButton.on_click = simulate
 
-
+    paramPanel.content.append(simulateButton)
 
     app.run()
 
