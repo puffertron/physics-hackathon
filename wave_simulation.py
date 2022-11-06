f249f453d6e18a233b5a8998e9f462d94e0cab15
Konicai <71294714+konicai@users.noreply.github.com>
2022-11-06 04:23:52 -0500

    Refactor style of params panel

--- wave_simulation.py
+++ wave_simulation.py
@@ -4,9 +4,6 @@ from parameters_panel import ParametersPanel
 import parameters
 import t_sim
 
-def doNothing():
-    pass
-
 app = Ursina(title="Wave Simulation")
 
 
@@ -14,7 +11,7 @@ app = Ursina(title="Wave Simulation")
 if __name__ == "__main__":
 
     parameters.initParams()
-    #paramPanel = ParametersPanel(doNothing)
+    #paramPanel = ParametersPanel(FUNCTION HERE)
 
     #ed = EditorCamera()
     #ed.position.y = 5
@@ -25,17 +22,8 @@ if __name__ == "__main__":
     cam.look_at(Vec3(0,0,0), 'forward')
     cam.rotation_z = 0
 
-
-
-    running:bool = True
-
     sim = t_sim.Simulation()
-    sim.begin() #get the visualiserss
+    sim.begin() #get the visualisers
 
-
-    #def update(): #This is our main loop that runs FOREVEEEERRRRR (or until running is False, but that never really happens, our code is eternal and shall never die)
-        #currentTickDistance += parameters.Instance.tick_distance
-        #sim.update(v, currentTickDistance)
-        #print(f"update frame: {currentTickDistance}")
     app.run()
 
