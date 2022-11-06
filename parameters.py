6516046e394daaf6050480ff24f55e621c9ea5ec
CiaranMcDJ <ciaranmcdj@gmail.com>
2022-11-06 03:57:53 -0500

    tiny changes to parameters to view problem easier

--- parameters.py
+++ parameters.py
@@ -6,16 +6,16 @@ Instance = None
 
 @dataclass
 class Parameters:
-    tick_distance: float = 1 #This is also in pixels, in simulation it doesn't look like it because z is skewed (already a TODO elesewhere to fix it)
+    tick_distance: float = 3 #This is also in pixels, in simulation it doesn't look like it because z is skewed (already a TODO elesewhere to fix it)
 
-    wavelength: float = 5 #Also in pixels haha, every length unit is in pixles
-    brightnessFactor: float = 1000000000000 #Turn up to make brighter pixels, will probably need to be a few hundred or more to see anything
+    wavelength: float = 0.1 #Also in pixels haha, every length unit is in pixles
+    brightnessFactor: float = 10 #Turn up to make brighter pixels, will probably need to be a few hundred or more to see anything
 
     occluder: Image = Image.open("images/2slit.png")
 
     visualizerAmount: int = 4
 
-    detectorDistance: float = 3
+    detectorDistance: float = 50
 
     lowResolution: int = 32 # For all planes in time simulation
     highResolution: int = 128  # For all planes in final state simulation
