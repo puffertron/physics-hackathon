32ac6ae35808046e332828ae844d6b60b253088b
CiaranMcDJ <ciaranmcdj@gmail.com>
2022-11-06 11:55:05 -0500

    added deprecation comments for final_state_sim

--- final_state_sim.py
+++ final_state_sim.py
@@ -1,6 +1,8 @@
 from parameters import Parameters
 from set_up_state import setUpFinalDetectorState
 
+
+#DEPRECATED - Final State Sim not actually used, we realized this wouldn't be much faster than t-sim with one visualizer
 def final_State_Simulate(param:Parameters, spaceToDrawOn:type): #Replace spaceToDraw with what it should be
     detectorState = setUpFinalDetectorState(param)
     
