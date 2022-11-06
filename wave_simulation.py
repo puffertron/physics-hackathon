65693270425a57ed0d47e550092b993f1e4636af
Konicai <71294714+konicai@users.noreply.github.com>
2022-11-05 20:08:30 -0400

    Update parameters type hint and update main

--- wave_simulation.py
+++ wave_simulation.py
@@ -1,14 +1,25 @@
-from ursina import *
+import dataclasses
 
+from ursina import *
 from parameters import Parameters
 
 app = Ursina(title="Wave Simulation")
 ed = EditorCamera
 
+__softParameters: Parameters = Parameters()
+parameters: Parameters = __softParameters
+
+
+def simulate():
+    global parameters
+    parameters = dataclasses.replace(__softParameters)
+
+
 if __name__ == "__main__":
     simulateButton = Button("Start Simulation")
+    simulateButton.on_click = simulate
+
+
 
     app.run()
-
-    params = Parameters()
 
