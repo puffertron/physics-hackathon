acd4b92bb2b02401a26ddef0db9a08884047ae0e
CiaranMcDJ <ciaranmcdj@gmail.com>
2022-11-06 01:24:19 -0400

    fixed a double variable

--- parameters.py
+++ parameters.py
@@ -9,7 +9,7 @@ class Parameters:
     tick_distance: float = 1
 
     wavelength: float = 5
-    brightnessFactor: float = 1000000000000
+    brightnessFactor: float = 1000000000000 #Turn up to make brighter pixels, will probably need to be a few hundred or more to see anything
 
     occluder: Image = Image.open("images/2slit.png")
 
@@ -20,7 +20,6 @@ class Parameters:
     lowResolution: int = 64  # For all planes in time simulation
     highResolution: int = 128  # For all planes in final state simulation
     
-    brightnessFactor:float = 1 #Turn up to make brighter pixels, will probably need to be a few hundred or more to see anything
 
     def printToConsole(self):
 
